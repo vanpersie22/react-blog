a347958b76bff6f72d43e633fff89443dfb3306c
John Doe <ezealionas@gmail.com>
2024-06-24 05:25:06 -0700

    header section

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,18 @@
+const flowbite = require("flowbite-react/tailwind");
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    // eslint-disable-next-line no-undef
+    flowbite.content(),
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [
+    flowbite.plugin(),
+  ],
+};
+
