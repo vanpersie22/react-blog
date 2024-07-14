c015fc8f180fa80e186b2c83c3bfdbeadfd818a8
John Doe <ezealionas@gmail.com>
2024-07-14 01:22:32 -0700

    post page functionalities

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,6 +13,7 @@ import PrivateRoute from './components/PrivateRoute'
 import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute'
 import CreatePost from './pages/CreatePost'
 import UpdatePost from './pages/UpdatePost'
+import PostPage from './pages/PostPage'
 
 
 export default function App() {
@@ -35,6 +36,7 @@ export default function App() {
         
       
         <Route path='/projects' element={<Projects />} />
+        <Route path='/post/:postSlug' element={<PostPage />} />
       </Routes>
       <Footer/>
     </BrowserRouter>
