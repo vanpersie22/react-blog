8915a7217d8fc3ecf0eaaed1b5382709bc4cddb5
John Doe <ezealionas@gmail.com>
2024-07-16 08:28:04 -0700

    add home.jsx functionalities

--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -11,16 +11,16 @@ const PostCard = ({ post }) => {
         duration-300 z-20' src={post.image} alt="post-cover" />
         </Link>
         <div className="p-3 flex flex-col gap-2">
-            <p className='text-md font-semibold line-clamp-2'>{post.title}</p>
-            <span className='italic text-sm'>{post.category}</span>
-            <Link to={`/post/${post.slug}`} className='z-10 group-hover:bottom-0
-            absolute bottom-[-200px] left-0 right-0 border border-green-500
-            text-green-500 dark:border-teal-500 dark:text-teal-500 hover:bg-green-500 hover:text-white 
-            dark:hover:bg-teal-500 dark:hover:text-white transition-all
-            duration-300 text-center py-2 rounded-md !rounded-tl-none m-2'>
-            Read More...
-            </Link>
-        </div>
+    <p className='text-md font-semibold line-clamp-2'>{post.title}</p>
+    <span className='italic text-sm bg-green-100 text-green-800 dark:bg-teal-700 dark:text-teal-100 py-1 px-2 rounded-full'>{post.category}</span>
+    <Link to={`/post/${post.slug}`} className='z-10 group-hover:bottom-0
+    absolute bottom-[-200px] left-0 right-0 border border-green-500
+    text-green-500 dark:border-teal-500 dark:text-teal-500 hover:bg-green-500 hover:text-white 
+    dark:hover:bg-teal-500 dark:hover:text-white transition-all
+    duration-300 text-center py-2 rounded-md !rounded-tl-none m-2'>
+    Read More...
+    </Link>
+</div>
         
     </div>
   )
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import CallToAction from '../components/CallToAction'
+import PostCard from '../components/PostCard'
+import { Button } from 'flowbite-react'
 
 const Home = () => {
+  const [posts, setPosts] = useState([]);
+
+
+  useEffect (() => {
+    const fetchPosts = async () => {
+      const res = await fetch('/api/post/getposts');
+      const data = await res.json();
+      setPosts(data.posts);
+    }
+    fetchPosts();
+  }, []);
+
   return (
-    <div>Home
+    <div>
+      <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto">
+        <h1 className='text-3xl font-bold lg:text-5xl'>Welcome to <span className='text-2xl font-bold lg:text-5xl text-green-500 dark:text-teal-500'>WriteAssignment</span> </h1>
+        <p className='text-gray-500 text-sm sm:text-sm'>Your ultimate destination for expert academic writing assistance and assignment support. Our mission is to provide you with the tools, resources, and guidance you need to excel in your academic journey.</p>
+
+        <Link to='/search' className='text-xs sm:text-sm text-green-500 dark:text-teal-500 font-bold 
+        hover:underline'>
+        View articles
+        </Link>
+      </div>
+      <div className="p-3 bg-amber-100 dark:bg-slate-800">
+        <CallToAction />
+      </div>
+
+      <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7">
+        {
+          posts && posts.length > 0 && (
+            <div className=" flex flex-col gap-6 mt-6">
+              <h2 className='text-2xl font-semibold text-center underline decoration-green-300 dark:decoration-teal-300 decoration-4 underline-offset-8'>Latest Articles</h2>
+              <div className="flex flex-wrap gap-4">
+                {
+                  posts.map((post)=>(
+                    <PostCard key={post._id} post={post} />    
+                  ))
+                }
+                
+              </div>
+              <Link to={'/search'} className='text-lg text-green-500 dark:text-teal-500 
+              hover:underline self-center mt-5'>
+             <Button className='bg-green-500 dark:bg-teal-500'> View more articles</Button>
+              </Link>
+            </div>
+          )
+        }
         
+      </div>
     </div>
   )
 }
