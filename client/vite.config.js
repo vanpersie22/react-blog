8eec366adc57c514084d382d0532fc31802e6a1e
Ezeali Jonas Arinze <ezealijonas@gmail.com>
2024-07-27 07:15:13 -0700

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'writeapi-heaxedgffbhdhccs.eastus-01.azurewebsites.net',
         secure: false,
       },
     },
